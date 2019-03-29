a969968d00bf2f51508419d65f825bddc0bb014c
Jose Carlos Recuero Arias <jose.recuero@gmail.com>
2019-03-29 13:56:28 -0700

    Include test for grafo path

--- test/grafo/test_grafo.py
+++ test/grafo/test_grafo.py
@@ -30,6 +30,47 @@ def test_add_edge():
     assert got == root, "Error: add edge: parent parent got {} exp {}".format(got, root)
 
 
+def test_grafo_path():
+    v1: Vertex = Vertex("v/1")
+    v2: Vertex = Vertex("v/2")
+    v3: Vertex = Vertex("v/3")
+    v4: Vertex = Vertex("v/4")
+    v5: Vertex = Vertex("v/5")
+    v6: Vertex = Vertex("v/6")
+    v7: Vertex = Vertex("v/7")
+    g: Grafo = Grafo("root/0")
+    g.add_edge(None, new_static_edge(None, v1, Link.DOWN))
+    e_v1_v2 = new_static_edge(v1, v2, Link.DOWN)
+    e_v1_v3 = new_static_edge(v1, v3, Link.DOWN)
+    e_v2_v3 = new_static_edge(v2, v3, Link.DOWN)
+    e_v2_v4 = new_static_edge(v2, v4, Link.DOWN)
+    e_v2_v5 = new_static_edge(v2, v5, Link.DOWN)
+    e_v3_v5 = new_static_edge(v3, v5, Link.DOWN)
+    e_v3_v6 = new_static_edge(v3, v6, Link.DOWN)
+    e_v4_v7 = new_static_edge(v4, v7, Link.DOWN)
+    e_v6_v7 = new_static_edge(v6, v7, Link.DOWN)
+    g.add_edge(v1, e_v1_v2)
+    g.add_edge(v1, e_v1_v3)
+    g.add_edge(v2, e_v2_v3)
+    g.add_edge(v2, e_v2_v4)
+    g.add_edge(v2, e_v2_v5)
+    g.add_edge(v3, e_v3_v5)
+    g.add_edge(v3, e_v3_v6)
+    g.add_edge(v4, e_v4_v7)
+    g.add_edge(v6, e_v6_v7)
+    paths = g.paths_from_v_to_v(v1, v7)
+    got = len(paths)
+    assert got == 3, "Error: paths: len got {} exp {}".format(got, 3)
+    exp = [
+        [e_v1_v2, e_v2_v3, e_v3_v6, e_v6_v7],
+        [e_v1_v2, e_v2_v4, e_v4_v7],
+        [e_v1_v3, e_v3_v6, e_v6_v7],
+    ]
+    for p in paths:
+        if p.edges not in exp:
+            assert False, "Error:paths: path {} not found".format(p)
+
+
 if __name__ == "__main__":
     log = get_loggar("grafo")
     v1: Vertex = Vertex("v/1")
@@ -43,7 +84,7 @@ if __name__ == "__main__":
     g.add_edge(None, new_static_edge(None, v1, Link.DOWN))
     g.add_edge(v1, new_static_edge(v1, v2, Link.DOWN))
     g.add_edge(v1, new_static_edge(v1, v3, Link.DOWN))
-    g.add_edge(v2, new_static_edge(v2, v3, Link.BI))
+    g.add_edge(v2, new_static_edge(v2, v3, Link.DOWN))
     g.add_edge(v2, new_static_edge(v2, v4, Link.DOWN))
     g.add_edge(v2, new_static_edge(v2, v5, Link.DOWN))
     g.add_edge(v3, new_static_edge(v3, v5, Link.DOWN))
@@ -52,5 +93,6 @@ if __name__ == "__main__":
     g.add_edge(v6, new_static_edge(v6, v7, Link.DOWN))
     log.Test(g.to_mermaid()).call()
     paths = g.paths_from_v_to_v(v1, v7)
+    # paths = g.paths_from_v_to_v(v5, v7)
     for p in paths:
         log.Test(p).call()
