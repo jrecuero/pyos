00015ab3cd65de18042d062d372013f6d13c8413
Jose Carlos Recuero Arias <jose.recuero@gmail.com>
2019-10-07 10:25:51 -0700

    Some comments

--- apps/sirtet/_game_tools.py
+++ apps/sirtet/_game_tools.py
@@ -1,5 +1,14 @@
+# Cell side for every piece.
 CELL_SIZE = 40
+
+# Board number of horizontal cells.
 XSIZE_CELLS = 9
+
+# Board number of vertical cells.
 YSIZE_CELLS = 16
+
+# Vertical threshold for end of match.
 YSIZE_THRESHOLD = YSIZE_CELLS - 3
+
+# Gravity time in miliseconds.
 GRAVITY_TIMER = 400
